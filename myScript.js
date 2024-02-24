a7a0b8df841db02852f60d2236f71c370277827b
Jedrick0923 <158731021+Jedrick0923@users.noreply.github.com>
2024-02-24 13:37:39 +0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,246 @@
+ 
+    let Title = "#Ode to Jed: The Web Weaver";
+    let TitleB = Title.bold();
+
+    let lin1 = "realm";
+    let lin1B = lin1.bold();
+
+    let lin2 = 'pixels';
+    let lin2B = lin2.bold();
+
+    let lin3 = 'code';
+    let lin3B = lin3.bold();
+
+    let lin4 = '**web weaver**';
+    let lin4B = lin4.bold();
+
+    let lin5 = 'pearls';
+    let lin5B = lin5.bold();
+
+
+    let H1 = '## I. The Blank Canvas';
+    let H1B = H1.bold();
+
+    let name = "Jed";
+    let nameB = name.bold();
+
+    let lin6 = '**alchemist of the browser**';
+    let lin6B = lin6.bold();
+    let lin6I = lin6B.italics();
+
+    let lin7 = 'keyboard';
+    let lin7B = lin7.bold();
+
+    let lin8 = 'wand';
+    let lin8B = lin8.bold();
+
+    let lin59 = 'pages';
+    let lin59B = lin59.bold();
+
+    let lin9 = 'Painting';
+    let lin9B = lin9.bold();
+
+
+    let H2 = '## II. The HTML Symphony';
+    let H2B = H2.bold();
+
+    let lin10 = 'fingers';
+    let lin10B = lin10.bold();
+
+    let lin11 = 'keys';
+    let lin11B = lin11.bold();
+
+    let lin12 = 'tags';
+    let lin12B = lin12.bold();
+
+    let lin13 = 'HTML';
+    let lin13B = lin13.bold();
+
+    let lin14 = 'symphonies';
+    let lin14B = lin14.bold();
+
+
+    let H3 = '## III. The CSS Enchanter';
+    let H3B = H3.bold();
+
+    let lin15 = '**CSS cloak**';
+    let lin15B = lin15.bold();
+    let lin15I = lin15B.italics();
+
+    let lin16 = 'Styling';
+    let lin16B = lin16.bold();
+
+    let lin17 = 'Colors bloom';
+    let lin17B = lin17.bold();
+
+    let lin18  = 'fonts sing';
+    let lin18B = lin18.bold();
+
+    let lin19 = 'evoke';
+    let lin19B = lin19.bold();
+
+    let lin69 = 'visual';
+    let lin69B = lin69.bold();
+
+
+    let H4 = '## IV. The JavaScript Magician';
+    let H4B= H4.bold();
+
+    let lin20 = 'magic';
+    let lin20B = lin20.bold();
+
+    let lin21 = '**JavaScript spells**';
+    let lin21B = lin21.bold();
+    let lin21I = lin21B.italics();
+
+    let lin22 = 'will';
+    let lin22B= lin22.bold();
+
+    let lin23 = 'Forms';
+    let lin23B = lin23.bold();
+
+    let lin24 = 'animations propel';
+    let lin24B  = lin24.bold();
+
+
+    let H5 = '## V. The Debugging Sage';
+    let H5B = H5.bold();
+
+    let lin25 = 'bugs';
+    let  lin25B= lin25.bold();
+
+    let lin26 = '**debugging spectacles**';
+    let lin26B = lin26.bold();
+    let lin26I = lin26B.italics();
+
+    let lin27 = 'glitches';
+    let lin27B  = lin27.bold();
+
+    let lin28 = "code's arc";
+    let lin28B = lin28.bold();
+
+    let lin29 = 'tames';
+    let lin29B = lin29.bold();
+
+    let lin30 = 'variables';
+    let lin30B = lin30.bold();
+
+
+    let H6 = '## VI. The Responsive Voyager';
+    let H6B = H6.bold();
+
+    let lin31 = 'Across';
+    let lin31B = lin31.bold();
+
+    let lin32 = 'devices';
+    let lin32B  = lin32.bold();
+
+    let lin33 = '**responsive sea**';
+    let lin33B = lin33.bold();
+    let lin33I = lin33B.italics();
+
+    let lin34 ='guide';
+    let lin34B = lin34.bold();
+
+    let lin35 = 'Adapting';
+    let lin35B= lin35.bold();
+
+    let lin36 = 'fluidity';
+    let lin36B = lin36.bold();
+
+
+    let H7 = '## VII. The SEO Whisperer';
+    let H7B  = H7.bold();
+
+    let lin37='labyrinth';
+    let lin37B = lin37.bold();
+
+    let lin38='**SEO whisperer**';
+    let lin38B = lin38.bold();
+    let lin38I = lin38B.italics();
+
+    let lin39='Meta tags';
+    let lin39B  = lin39.bold();
+
+    let lin40='wanderers';
+    let lin40B  = lin40.bold();
+
+
+    let H8 = '## VIII. The Git Alchemist';
+    let H8B = H8.bold();
+
+    let lin41='mystical potion';
+    let lin41B = lin41.bold();
+
+    let lin42="Git's";
+    let lin42B = lin42.bold();
+
+    let lin43='merge';
+    let lin43B  = lin43.bold();
+
+    let lin44='repository';
+    let lin44B = lin44.bold();
+
+    let lin45='code';
+    let lin45B= lin45.bold();
+
+
+    let H9 = '## IX. The Deployment Dreamer';
+    let H9B = H9.bold();
+
+    let lin46='moon';
+    let lin46B = lin46.bold();
+
+    let lin47='world';
+    let lin47B = lin47.bold();
+
+    let lin48='Deployments';
+    let lin48B = lin48.bold();
+
+    let lin49='website';
+    let lin49B = lin49.bold();
+
+    let lin50='unfurled';
+    let lin50B = lin50.bold();
+
+
+    let H10 = "## X. The Web Developer's Anthem";
+    let H10B = H10.bold();
+
+    let lin51='**web developer**';
+    let lin51B = lin51.bold();
+
+    let lin52='pixels';
+    let lin52B = lin52.bold();
+
+    let lin53='clicks';
+    let lin53B = lin53.bold();
+
+    let lin54='binary';
+    let lin54B = lin54.bold();
+
+    let lin55='matrix';
+    let lin55B = lin55.bold();
+
+
+    let lin56 = '*Jed, may your lines be bug-free,';
+    let lin56I = lin56.italics();
+
+    lin57 = 'May your CSS always cascade gracefully,';
+    let lin57I = lin57.italics();
+
+    lin58 = 'And may your JavaScript forever flow,';
+    let lin58I = lin58.italics();
+
+    lin60 = "As you navigate the web's eternal ebb and flow.*";
+    let lin60I = lin60.italics();
+
+
+
+
+document.getElementById("demo").innerHTML = TitleB + "<br><br>" + "In the " + lin1B + ' of ones and zeros,' + "<br>" + "Where " + lin2B + " dance and " + lin3B + 
+' unfurls,' + "<br>" + "There resides a silent sorcerer," + "<br>" + "A " + lin4B + " with dreams and " + lin5B + "." + "<br><br>" + H1B + "<br><br>" + nameB + ", the " + lin6I + "," + "<br>" + "Wields his " + lin7B + " like a " + lin8B + "," + "<br>" + "He conjures " + lin59B + " from the thin air," + "<br>" + lin9B + " landscapes with lines so fond." + "<br><br>" + H2B + "<br><br>" + "His " + lin10B + " dance upon the " + lin11B + "," + "<br>" + "Crafting " + lin12B + " like musical notes," + "<br>" + lin13B + " whispers secrets to the breeze," + "<br>" + "As " + nameB + " orchestrates " + lin14B + " in quotes." + "<br><br>" + H3B + "<br><br>" + "Next, he dons his " + lin15I + "," + "<br>" + lin16B + " elements with flair and grace," + "<br>" + lin17B + ", " + lin18B + ", layouts " + lin19B + "," + "<br>" + "A " + lin69B  + " ballet in this digital space." + "<br><br>" + H4B + "<br><br>" + "But wait, the " + lin20B + " deepens still," + "<br>" + nameB + " summons " + lin21I + "," + "<br>" + "Interactivity blooms at his " + lin22B + "," + "<br>" + lin23B + " validate, " + lin24B + "." + "<br><br>" + H5B + "<br><br>" + "Yet, " + lin25B + " creep in like shadows dark, " + "<br>" + nameB + " dons his " + lin26I + "," + "<br>" + "Chasing " + lin27B + " through the " + lin28B + "," + "<br>" + "He " + lin29B + " wild loops and elusive " + lin30B + "." + "<br><br>" + H6B + "<br><br>" + lin31B + " screens, " + lin32B + "," + " and dimensions," + "<br>" + nameB + " sails on the " + lin33I + ',' + "<br>" + "Media queries " + lin34B + " his intentions," + "<br>" + lin35B + " layouts with " + lin36B + '.' + "<br><br>" + H7B + "<br><br>" + "In the " + lin37B + " of search engines," + "<br>" + nameB + " becomes an " + lin38I + "," + "<br>" + lin39B + ", keywords, backlink strings," + "<br>" + "He crafts pathways to digital " + lin40B + "<br><br>" + H8B + "<br><br>" + "Version control, a " + lin41B + "," + "<br>" + nameB + " brews commits in " + lin42B + " cauldron" + "<br>" + "Branches fork, " + lin43B + " conflicts in motion," + "<br>" + "His " + lin44B + " a saga of " + lin45B + " reborn." + "<br><br>" + H9B + "<br><br>" + "And when the " + lin46B + " kisses the horizon," + "<br>" + nameB + " unfurls his creation to the " + lin47B + "," + "<br>" + lin48B + " like shooting stars, arisen," + "<br>" + "His " + lin49B + " sails, its sails " + lin50B + '.' + "<br><br>" + H10B + "<br><br>" + "So here's to " + nameB + ", the " + lin51B + ',' + "<br>" + "A poet of " + lin52B + ", a composer of " + lin53B + "," + "<br>" + "In the " + lin54B + " symphony, he's the reveler," + "<br>" + "Weaving dreams into the digital " + lin55B + "<br><br><br>" + lin56I + "<br>" + lin57I + "<br>" + lin58I + "<br>" + lin60I
+
+
+
+;
